7ad967b7278be6db19da4748099699d4fe6f8a40
ReTr0hxh <retr0zueiro@gmail.com>
2025-05-08 01:33:52 +0000

    xadrez

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int i = 0;
+    int i;
     int opcao, opcao2;
 
     //escolhendo a peça
@@ -10,6 +10,8 @@ int main() {
     printf("1 - Torre \n");
     printf("2 - Bispo \n");
     printf("3 - Rainha \n");
+    printf("4 - Cavalo \n");
+    printf("5 - Sair...\n");
     printf("Digite uma opção \n");
     scanf("%d", &opcao);
 
@@ -27,6 +29,7 @@ int main() {
         scanf("%d", &opcao2);
 
         //movimento da peça
+        i = 0;
         switch (opcao2)
         {
         case 1:
@@ -57,9 +60,8 @@ int main() {
             printf("Opção invalida \n");
             break;
         }
-
-
         break;
+        //escolhendo a posiçao que a peça vai se movimentar
     case 2:
         printf("Peça escolhida: Bispo \n");
         printf("Escolha a posição que a peça vai se movimentar \n");
@@ -71,7 +73,7 @@ int main() {
         scanf("%d", &opcao2);
 
         //movimento da peça
-
+        i = 0;
         switch (opcao2)
         {
         case 1:
@@ -106,8 +108,8 @@ int main() {
             printf("Opção invalida \n");
         break;
        }
-
         break;
+        //escolhendo a posiçao que a peça vai se movimentar
     case 3:
         printf("Peça escolhida: Rainha \n");
         printf("Escolha a posição que a peça vai se movimentar \n");
@@ -123,7 +125,7 @@ int main() {
         scanf("%d", &opcao2);
 
         //movimento da peça
-
+        i = 0;
         switch (opcao2)
         {
         case 1:
@@ -186,13 +188,75 @@ int main() {
             printf("Opção invalida \n");
         break;
        }
+        break;
+       //escolhendo a posiçao que a peça vai se movimentar
+    case 4:
+        printf("Peça escolhida: Cavalo \n");
+        printf("Escolha a posição que a peça vai se movimentar \n");
+        printf("1 - cima direita \n");
+        printf("2 - baixo direita \n");
+        printf("3 - cima esquerda \n");
+        printf("4 - baixo esquerda \n");
+        scanf("%d", &opcao2);
 
-        break;          
+        //movimento da peça
+        switch (opcao2)
+        {
+        case 1:
+            for (int i = 1; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    printf("cima \n");
+                }
+                printf("Direita \n");
+            }
+            break;
+        case 2:
+            for (int i = 1; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    printf("baixo \n");
+                }
+                printf("Direita \n");
+            }
+            break;
+        case 3:
+            for (int i = 1; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    printf("cima \n");
+                }
+                printf("esquerda \n");
+            }
+            break;
+        case 4:
+            for (int i = 1; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    printf("baixo \n");
+                }
+                printf("esquerda \n");
+            }
+            break;
+        
+        default:
+            printf("Opção invalida! \n");
+            break;
+        }
+        break;
+    case 5:
+        printf("Saindo... \n");
+        return 0;
+          
     default:
         printf("Opção invalida! \n");
         break;
     }
     
-    
+
     return 0;
 }
